acfaf6f2bc1f83468c480f578534b8ba54ae1462
Carolina Navarrete <100876091+canavarrete01@users.noreply.github.com>
2024-09-13 13:49:23 -0400

    Update check_brewster.py

--- check_brewster.py
+++ check_brewster.py
@@ -1,7 +1,7 @@
 import code_test as ct
 import numpy as np
 
-xpath = '/Volumes/DudleyDisk/LineLists/'
+xpath = '/mendel-nas1/cnavarrete/linelists/'
 xlist = 'gaslistR10K.dat'
 results = [ct.NoCloud_Tdwarf(xpath,xlist),ct.MieClouds_Ldwarf(xpath,xlist)]
 
